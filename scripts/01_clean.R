5ab189685add5641f04252a3e4ce953e464ac573
kixt <petersen.thore@gmail.com>
2019-09-30 16:19:44 +0200

    updated data file

--- scripts/01_clean.R
+++ scripts/01_clean.R
@@ -5,7 +5,8 @@
 # Thore Petersen
 # September 2019
 
+setwd("/Users/thore/Documents/Studium/Master/Semester5/Financial Econometrics/term paper/fin_econ_analysis")
 
 # Import data -------------------------------------------------------------
 
-
+test_imp <- read.csv("./data/lse.csv")
