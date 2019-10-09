34a2e2611936ccb833460370ef3471b866ceb8b4
kixt <petersen.thore@gmail.com>
2019-10-09 16:48:03 +0200

    finalise ARCH(q) implementation

--- scripts/julia_src/arch.jl
+++ scripts/julia_src/arch.jl
@@ -1,10 +1,10 @@
-function arch(x::Float64, p::Int64, θ0::Float64)
+function arch(x::Float64, q::Int64, θ0::Float64)
     #=
-        function to estimate an ARCH(p) model on x, where X_t = σ_t * ϵ_t,
-        with ϵ_t ~ N(0, 1)
+        function to estimate an ARCH(q) model on x, where X_t = σ_t * ϵ_t,
+        with ϵ_t ~ N(0,1)
         Args:
             x: data vector
-            p: lag order of the ARCH model
+            q: lag order of the ARCH model
             θ0: initial parameters for optimisation procedure
         Returns:
             Optim output
@@ -12,25 +12,25 @@ function arch(x::Float64, p::Int64, θ0::Float64)
 
     function nll(θ::Float64)::Float64
         #=
-            function to calculate the negative log-likelihood value of an ARCH(p)
+            function to calculate the negative log-likelihood value of an ARCH(q)
             model at θ
-            N, x, σ2, p of the ARCH(p) are required in the parent environment
+            N, x, σ2, q of the ARCH(q) are required in the parent environment
             σ2 in the parent environment is mutated by this function
             Args:
-                θ: vector of parameters [ω, α_1, ..., α_p] at which to evaluate
+                θ: vector of parameters [ω, α_1, ..., α_q] at which to evaluate
             Returns:
                 the numerical value of the negative logL
         =#
 
         ω = θ[1]
-        α = θ[collect(2:(p + 1))]
+        α = θ[collect(2:(q + 1))]
 
         # initialise logL sum
         ll = 0
 
-        for t in (p + 1):N
+        for t in (q + 1):N
             # collect indices for correct temporal subsetting
-            t_i = collect((t - 1):-1:(t - p))
+            t_i = collect((t - 1):-1:(t - q))
             # estimate conditional variance at t
             σ2[t] = ω + α' * x2[t_i]
             # add value of logL at time t to the sum
@@ -44,26 +44,26 @@ function arch(x::Float64, p::Int64, θ0::Float64)
     function ∇nll!(G::Float64, θ::Float64)
         #=
             function to evaluate the gradient of the negative log-likelihood of
-            an ARCH(p) model
-            N, x, σ2, p of the ARCH(p) are required in the parent environment
+            an ARCH(q) model
+            N, x, σ2, q of the ARCH(q) are required in the parent environment
             Args:
                 G: fixed-size storage array that is mutated by the function; to
                     avoid creating a new array with every function call
-                θ: vector of parameters [ω, α_1, ..., α_p] at which to evaluate
+                θ: vector of parameters [ω, α_1, ..., α_q] at which to evaluate
             Returns:
                 a mutation of G with the numerical values of the gradient
                 evaluated at θ
         =#
 
         ω = θ[1]
-        α = θ[collect(2:(p + 1))]
+        α = θ[collect(2:(q + 1))]
 
         # initialise σ^2 with unconditional variance
         σ2 = fill(1 / N * sum(x2), N)
 
         # calculate conditional variance
-        for t in (p + 1):N
-            t_i = collect((t - 1):-1:(t - p))
+        for t in (q + 1):N
+            t_i = collect((t - 1):-1:(t - q))
             σ2[t] = ω + α' * x2[t_i]
         end
 
@@ -85,7 +85,7 @@ function arch(x::Float64, p::Int64, θ0::Float64)
         end
 
         # mutate G with numerical values of the partial derivates
-        for i in 2:(p + 1)
+        for i in 2:(q + 1)
             G[i] = ∇_helper(i - 1)
         end
     end
@@ -96,12 +96,12 @@ function arch(x::Float64, p::Int64, θ0::Float64)
     # square the zero mean series x
     x2 = x.^2
 
-    # initialise with unconditional variance to not have 0 for the first p elements
+    # initialise with unconditional variance to not have 0 for the first q elements
     σ2 = fill(1 / N * sum(x2), N)
 
     # define parameter bounds, 0 < θ < Inf
-    lower = zeros(p + 1)
-    upper = fill(Inf, p + 1)
+    lower = zeros(q + 1)
+    upper = fill(Inf, q + 1)
     inner_optimizer = LBFGS()
     res = optimize(nll, ∇nll!, lower, upper, θ0, Fminbox(inner_optimizer))
 
