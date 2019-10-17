0a25934262555ac0c5bce5ee5d2e2ea62797b2be
kixt <petersen.thore@gmail.com>
2019-10-17 17:36:42 +0200

    skew-t fitting trials for GARCH residuals

--- scripts/02_margins_trials.R
+++ scripts/02_margins_trials.R
@@ -7,6 +7,8 @@
 
 library(data.table)
 library(rugarch)
+library(sn) # skew distributions
+library(fitdistrplus) # extends MASS::fitdistr(); probably overkill, we'll see
 library(ggplot2)
 
 
@@ -15,15 +17,16 @@ library(ggplot2)
 load("./data/clean.RData")
 setkey(dt, iso3c, Date)
 
+
 # Set up GARCH specification ----------------------------------------------
 
 markets <- unique(dt$iso3c)
+n <- length(markets)
 
-# set up recursive list for specifications and results of GARCH estimation
-garch <- vector("list", 2)
-names(garch) <- c("specs", "results")
-garch[[1]] <- garch[[2]] <- vector("list", length(markets))
-names(garch[[1]]) <- names(garch[[2]]) <- markets
+# set up lists for specifications and results of GARCH estimation
+garch_spec <- vector("list", n)
+garch_fit <- vector("list", n)
+names(garch_spec) <- names(garch_fit) <- markets
 
 test_spec <- ugarchspec(
   variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
@@ -33,14 +36,110 @@ test_spec <- ugarchspec(
 # use e.g. fixed.pars = list(shape = 5) to set dof of t distribution.model, 
 # otherwise estimted via ML
 
+# estimate GARCH models
 for(m in markets) {
-  x <- dt[m, ret]
-  garch[[2]][[m]] <- ugarchfit(test_spec, x, solver = "hybrid")
-}; rm(m, x)
-
+  garch_spec[[m]] <- test_spec
+  
+  # select rows in data.table by key, fast binary search
+  garch_fit[[m]] <- ugarchfit(garch_spec[[m]], dt[m, ret], solver = "hybrid")
+}
 
 
 # Extract GARCH residuals -------------------------------------------------
 
-dt[, res := unlist(lapply(garch[[2]], residuals, standardize = TRUE))]
+# extract standardized residuals, i.e. epsi_t / sig_t|sig_{t-1}
+dt[, res := unlist(lapply(garch_fit, residuals, standardize = TRUE))]
+
+# # KDE for residuals
+# res_kde <- vector("list", n)
+# names(res_kde) <- markets
+# 
+# for(m in markets) {
+#   res_kde[[m]] <- density(dt[m, res])
+# }
+
+ggplot(dt) +
+  geom_density(aes(x = res)) +
+  facet_wrap(iso3c ~ .)
+
+# ECDF of residuals
+res_ecdf <- vector("list", n)
+names(res_ecdf) <- markets
+
+dt[, eqtl := NA_real_]
+
+for(m in markets) {
+  res_ecdf[[m]] <- ecdf(dt[m, res])
+  
+  # extract empirical quantile of residuals per country
+  dt[m, eqtl := res_ecdf[[m]](res)]
+}
+
+# create data.table of empirical quantiles to check dependence
+eqtls <- dcast(dt, Date ~ iso3c, value.var = "eqtl")
+
+# characteristic pattern for DEU--FRA, almost linear, with clustering in corners,
+# i.e. strong tail dependence
+ggplot(eqtls, aes(x = FRA, y = DEU)) + 
+  geom_point()
+
+
+# Fitting distributions to residuals -- skew-t ----------------------------
+
+pst <- function(q, xi=0, omega=1, alpha=0, nu=Inf, dp=NULL, method=0) {
+  # wrapper for sn::pst() to use in fitdistrplus::fitdist(), which has specific 
+  # requirements for the pst() function:
+  #   - first argument must be named q
+  #   - return NULL when q = NULL (sn::pst() throws an error)
+  #   - names must match exactly (sn::pst() allows ... for functionality I don't 
+  #   need, thus does not throw "unused arguments" error)
+  
+  if(length(q) == 0) return(NULL)
+  sn::pst(x = q, xi, omega, alpha, nu, dp, method)
+}
+
+# fit skewed-t-distribution to all residuals
+fit_st <- vector("list", n)
+names(fit_st) <- markets
+for(m in markets) {
+  fit_st[[m]] <- fitdist(dt[m, res], "st",
+                         start = list(xi = 0, omega = 1, alpha = 0, nu = 5))
+}
+
+gen_dens <- function(fit, grp_name, grp_id, n = 100) {
+  # function to generate points to plot density estimate in fit, designed to 
+  # generate data to overlay in ggplot() + facet_wrap(grp_name ~ .) instead of
+  # stat_function()
+  # 
+  # Args:
+  #   fit: object returned from fitdistrplus::fitdist()
+  #   grp_name: column name of the group identifiers in original data
+  #   grp_id: group identifier for which fit was estimated
+  # Returns:
+  #   a data.table with a grid of x, the corresponding density, and the group
+  
+  if(class(fit) != "fitdist") stop("fit must be class fitdist")
+  
+  dfn <- paste0("d", fit$distname)
+  dargs <- split(unname(fit$estimate), names(fit$estimate))
+  
+  grid <- data.table(x = with(fit, seq(min(data), max(data), length = n)))
+  grid[, density := do.call(dfn, c(list(x = x), dargs))]
+  grid[, eval(grp_name) := grp_id]
+  
+  return(grid)
+}
+
+# create data.table for densities to overlay on plot
+densities <- vector("list", n)
+names(densities) <- markets
+for(m in markets) {
+  densities[[m]] <- gen_dens(fit_st[[m]], "iso3c", m)
+}
+densities <- rbindlist(densities)
+
+ggplot() +
+  geom_density(aes(x = res), dt) +
+  geom_line(aes(x = x, y = density), densities, colour = "red") +
+  facet_wrap(iso3c ~ .)
 
