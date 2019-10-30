25f8a9ebbc22f47085cc909422685435f3b954fa
kixt <petersen.thore@gmail.com>
2019-10-30 20:19:38 +0100

    plot joint KDE of residuals

--- scripts/05_visualise.R
+++ scripts/05_visualise.R
@@ -31,10 +31,52 @@ for(i in 1:(n - 1)) {
     dt_sub <- dcast(dt[.(c(m1, m2))], Date ~ iso3c, value.var = "eqntl")
     dt_sub <- zoo::na.trim(dt_sub)
     
-    # estime bivariate KDE
+    # estimate bivariate KDE
     # need to include limits to make grid evenly spaced for geom_tiles()
     kde_tmp <- kde2d(dt_sub[, get(m1)], dt_sub[, get(m2)], n = 100, 
                      lims = c(0, 1, 0, 1))
+    
+    dt_tmp <- expand.grid(x = kde_tmp$x, y = kde_tmp$y)
+    dt_tmp$z <- as.numeric(kde_tmp$z) # unwraps matrix
+    
+    dt_tmp$pair <- paste0(m1, "-", m2) # name of country pair
+    
+    # index for storage in list
+    if(i == 1 && j == 2) {
+      ls_i <- 1
+    } else {
+      ls_i <- ls_i + 1
+    }
+    
+    kde_ls[[ls_i]] <- dt_tmp
+  }
+}; rm(m1, m2, i, j, ls_i, dt_tmp, dt_sub, kde_tmp)
+
+eq_kde_dens <- rbindlist(kde_ls)
+
+ggplot(eq_kde_dens, aes(x, y, fill = z)) +
+  geom_tile() +
+  scale_fill_viridis() +
+  theme_minimal() +
+  facet_wrap(pair ~ .)
+
+
+# Heatmap of joint density estimate ---------------------------------------
+
+kde_ls <- vector("list", n * (n - 1) / 2)
+for(i in 1:(n - 1)) {
+  for(j in (i + 1):n) {
+    m1 <- markets[i]
+    m2 <- markets[j]
+    
+    # reshape data and trim to pairwise complete observations
+    dt_sub <- dcast(dt[.(c(m1, m2))], Date ~ iso3c, value.var = "res")
+    dt_sub <- zoo::na.trim(dt_sub)
+    
+    # estimate bivariate KDE
+    # need to include limits to make grid evenly spaced for geom_tiles()
+    kde_tmp <- kde2d(dt_sub[, get(m1)], dt_sub[, get(m2)], n = 100, 
+                     lims = c(-5, 5, -5, 5))
     
     dt_tmp <- expand.grid(x = kde_tmp$x, y = kde_tmp$y)
     dt_tmp$z <- as.numeric(kde_tmp$z) # unwraps matrix
@@ -60,8 +102,6 @@ ggplot(kde_dens, aes(x, y, fill = z)) +
   theme_minimal() +
   facet_wrap(pair ~ .)
 
-
-
-
-
+# not very instructive, cut off super heavy tails in limits for KDE, otherwise 
+# plots hardly legible; other than that high variation per country, so dim 
 
