88454dfe617dac7386912b17ce82722be6bd4a18
kixt <petersen.thore@gmail.com>
2019-10-30 20:02:46 +0100

    visualise pairwise dependence

---
+++ scripts/05_visualise.R
@@ -0,0 +1,70 @@
+#####################################
+# Financial Econometrics Term Paper #
+#####################################
+
+# Thore Petersen
+# October 2019
+
+library(data.table)
+library(ggplot2)
+library(MASS)
+library(viridis)
+
+
+# Load data ---------------------------------------------------------------
+
+load("./data/tmp/03_tmp.RData")
+
+# calculate empirical quantiles
+dt[, eqntl := copula::pobs(res), by = iso3c]
+
+
+# Heatmap of empirical quantiles ------------------------------------------
+
+# calculate 2D KDEs for all country pairs, returns density estimates over grid 
+# to plot in heatmap
+
+kde_ls <- vector("list", n * (n - 1) / 2)
+for(i in 1:(n - 1)) {
+  for(j in (i + 1):n) {
+    m1 <- markets[i]
+    m2 <- markets[j]
+    
+    # reshape data and trim to pairwise complete observations
+    dt_sub <- dcast(dt[.(c(m1, m2))], Date ~ iso3c, value.var = "eqntl")
+    dt_sub <- zoo::na.trim(dt_sub)
+    
+    # estime bivariate KDE
+    # need to include limits to make grid evenly spaced for geom_tiles()
+    kde_tmp <- kde2d(dt_sub[, get(m1)], dt_sub[, get(m2)], n = 100, 
+                     lims = c(0, 1, 0, 1))
+    
+    dt_tmp <- expand.grid(x = kde_tmp$x, y = kde_tmp$y)
+    dt_tmp$z <- as.numeric(kde_tmp$z) # unwraps matrix
+    
+    dt_tmp$pair <- paste0(m1, "-", m2) # name of country pair
+    
+    # index for storage in list
+    if(i == 1 && j == 2) {
+      ls_i <- 1
+    } else {
+      ls_i <- ls_i + 1
+    }
+    
+    kde_ls[[ls_i]] <- dt_tmp
+  }
+}; rm(m1, m2, i, j, ls_i, dt_tmp, dt_sub, kde_tmp)
+
+kde_dens <- rbindlist(kde_ls)
+
+ggplot(kde_dens, aes(x, y, fill = z)) +
+  geom_tile() +
+  scale_fill_viridis() +
+  theme_minimal() +
+  facet_wrap(pair ~ ., scales = "free")
+
+
+
+
+
+
