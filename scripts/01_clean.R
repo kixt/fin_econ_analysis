7584cc5ab2bac58ee6606653a2beb3b8e5326074
kixt <petersen.thore@gmail.com>
2019-10-31 15:21:16 +0100

    switch to price returns

--- scripts/01_clean.R
+++ scripts/01_clean.R
@@ -12,7 +12,7 @@ library(zoo)
 
 # Import and prepare data -------------------------------------------------
 
-mb_imp <- read.xlsx("./data/raw/macrobond.xlsx", 
+mb_imp <- read.xlsx("./data/raw/macrobond.xlsx", sheet = "price_return", 
                     na.strings = "NaN", detectDates = TRUE)
 setDT(mb_imp)
 mb_tmp <- melt(mb_imp, id.vars = "Date")
@@ -23,8 +23,13 @@ mb_tmp[, variable := gsub("[.]", " ", variable)]
 mb_tmp[, iso3c := countrycode::countrycode(variable, "country.name", "iso3c")]
 # extract currency information
 mb_tmp[, currency := stringr::str_extract(variable, "EUR|GBP|USD|NOK")]
-
 setkey(mb_tmp, iso3c, currency, Date)
+
+mb_tmp["DEU", index := "DAX30"]
+mb_tmp["FRA", index := "CAC40"]
+mb_tmp["IRL", index := "ISEQ"]
+mb_tmp["ITA", index := "MIB"]
+mb_tmp["USA", index := "SP500"]
 
 # calculate returns
 mb_tmp[, ret := c(NA_real_, diff(log(value))), by = variable]
@@ -42,6 +47,7 @@ mb_fin <- mb_tmp2[currency %in% c("EUR", "USD")]
 mb_fin <- mb_fin[!(iso3c %in% c("GBR", "GRC"))]
 
 mb_fin[, c("variable", "value") := NULL]
+mb_fin[, c("currency") := NULL]
 
 # Date of official EURO introduction and fixation of intra-European exch. rates
 mb_fin <- mb_fin[Date >= as.Date("1999-01-01")]
