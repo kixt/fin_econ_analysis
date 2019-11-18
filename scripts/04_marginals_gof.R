152d6c073eccfdd2fb94b0d4e58135f7e7346081
kixt <petersen.thore@gmail.com>
2019-11-18 15:46:52 +0100

    Apply Cramér-von Mises GoF test

--- scripts/04_marginals_gof.R
+++ scripts/04_marginals_gof.R
@@ -9,6 +9,7 @@
 # determine best fit and export data
 
 library(data.table)
+library(goftest)
 
 
 # Load data ---------------------------------------------------------------
@@ -19,16 +20,16 @@ load("./data/tmp/03_tmp_stsged.RData")
 
 # Calculate MSE -----------------------------------------------------------
 
-colnames(dt_gpd)[which(colnames(dt_gpd) == "qntl")] <- "d_gpd"
-colnames(dt_stsged)[which(colnames(dt_stsged) == "qntl")] <- "d_stsged"
+colnames(dt_gpd)[which(colnames(dt_gpd) == "Fh")] <- "Fh_gpd"
+colnames(dt_stsged)[which(colnames(dt_stsged) == "Fh")] <- "Fh_stsged"
 
 dt <- merge(dt_gpd, dt_stsged, 
             by = intersect(colnames(dt_gpd), colnames(dt_stsged)))
 setkey(dt, iso3c, Date)
 rm(dt_gpd, dt_stsged)
 
-dt[, c("mse_gpd", "mse_stsged") := .(mean((eqntl - d_gpd)^2), 
-                                     mean((eqntl - d_stsged)^2)), 
+dt[, c("mse_gpd", "mse_stsged") := .(mean((eFh - Fh_gpd)^2), 
+                                     mean((eFh - Fh_stsged)^2)), 
    by = .(iso3c, tail)]
 
 # MSE in lower tail always lower for skew-t/skew-GED compared to GPD; in upper tail
@@ -36,9 +37,22 @@ dt[, c("mse_gpd", "mse_stsged") := .(mean((eqntl - d_gpd)^2),
 dt[, mean(mse_gpd - mse_stsged), by = .(iso3c, tail)]
 
 
+# Cramér-von Mises test ---------------------------------------------------
+
+# apply CvM test in the tails (only there, otherwise mixture of ecdf part in 
+# mixed GPD fit kind of cheating)
+
+dt[tail == "lower", cvm.test(Fh_gpd / q), by = .(iso3c)]
+dt[tail == "lower", cvm.test(Fh_stsged / q), by = .(iso3c)]
+
+dt[tail == "upper", cvm.test((Fh_gpd - 1) / q + 1), by = .(iso3c)]
+dt[tail == "upper", cvm.test((Fh_stsged - 1) / q + 1), by = .(iso3c)]
+
+
 # Export data -------------------------------------------------------------
 
-dt[, c("d_gpd", "mse_gpd", "mse_stsged") := NULL]
-colnames(dt)[which(colnames(dt) == "d_stsged")] <- "qntl"
+dt[, c("Fh_stsged", "mse_gpd", "mse_stsged") := NULL]
+colnames(dt)[which(colnames(dt) == "Fh_gpd")] <- "Fh"
 
 save(dt, markets, n, file = "./data/tmp/04_tmp.RData")
+
