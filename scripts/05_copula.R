7cf928847ecd9019cd0324635e33ae39c3263ffe
kixt <petersen.thore@gmail.com>
2019-11-25 19:38:19 +0100

    Length of pairwise overlapping periods.

--- scripts/05_copula.R
+++ scripts/05_copula.R
@@ -221,3 +221,18 @@ load("./data/tmp/gof_res.RData")
 lapply(gof$low, function(x) x@copula@fitting.stats$convergence)
 
 lapply(best_cop$low, function(x) x[[1]]@fitting.stats$convergence)
+
+
+
+for(m1 in markets) {
+  for(m2 in markets) {
+    for(v in c(T, F)) {
+      print(dt[, 
+         length(intersect(
+           .SD[which(iso3c == m1 & high_vola == v), Date], 
+           .SD[which(iso3c == m2 & high_vola == v), Date]
+           ))
+         ])
+    }
+  }
+}
