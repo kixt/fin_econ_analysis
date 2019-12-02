dcf978dfd0a83c7d591c3560516d0ceedcdef230
kixt <petersen.thore@gmail.com>
2019-12-02 17:19:51 +0100

    Copula results to LaTeX

--- scripts/07_copula_gof.R
+++ scripts/07_copula_gof.R
@@ -13,7 +13,7 @@ library(copula)
 # Load data ---------------------------------------------------------------
 
 load("./data/tmp/04_tmp.RData")
-load("./data/tmp/best_cop.RData")
+# load("./data/tmp/best_cop.RData")
 
 cop_res <- setClass(
   "cop_res", slots = list(
@@ -23,6 +23,7 @@ cop_res <- setClass(
 )
 
 load("./data/tmp/gof_res.RData")
+load("./data/tmp/gof_av_res.RData")
 
 
 # Goodness of Fit ---------------------------------------------------------
@@ -30,7 +31,7 @@ load("./data/tmp/gof_res.RData")
 
 lapply(gof$low, function(x) x@copula@fitting.stats$convergence)
 
-lapply(best_cop$high, function(x) x[[1]]@fitting.stats$convergence)
+# lapply(best_cop$high, function(x) x[[1]]@fitting.stats$convergence)
 
 
 
@@ -48,4 +49,102 @@ for(m1 in markets) {
 }
 
 lapply(gof_res, function(x) lapply(x, function(y) y$p.value))
+lapply(gof_av_res, function(y) y$p.value)
 
+
+# Prepare table for LaTeX -------------------------------------------------
+
+gof_res_row <- function(htest, pair, regime) {
+  # function to generate row for LaTeX results table of GoF results
+  # Args:
+  #   htest: object of class htest containing the GoF results
+  #   pair: character, name of country pair
+  #   regime: character, volatility regime
+  # Returns:
+  #   data.table containing the results
+  
+  c <- stringr::str_extract(
+    htest$method, 
+    "t-copula|Gumbel|Frank|Clayton|Frnc, Gmbc, Clyc|Frnc, t-cp, Gmbc, Clyc"
+    )
+  if(!(c %in% c("Gumbel", "Frank", "Clayton"))) {
+    c <- switch(
+      c, 
+      "t-copula" = "t",
+      "Frnc, Gmbc, Clyc" = "m_nt",
+      "Frnc, t-cp, Gmbc, Clyc" = "m_t"
+    ) 
+  }
+  
+  res <- data.table(
+    pair = pair, regime = regime, pi_F = NA_real_, pi_G = NA_real_, 
+    pi_C = NA_real_, pi_t = NA_real_, alpha = NA_real_, delta = NA_real_, 
+    theta = NA_real_, rho = NA_real_, nu = NA_real_, p = NA_real_
+  )
+  
+  res <- melt(res, id.vars = c("pair", "regime"))
+  setkey(res, variable)
+  
+  if(c == "t") {
+    res[
+      c("pi_t", "rho", "nu", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Gumbel") {
+    res[
+      c("pi_G", "delta", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Clayton") {
+    res[
+      c("pi_C", "theta", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Frank") {
+    res[
+      c("pi_G", "alpha", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "m_nt") {
+    res[
+      c("alpha", "delta", "theta", "pi_F", "pi_G", "pi_C", "p"), 
+      value := c(htest$parameter, htest$p.value)
+      ]
+  } else if(c == "m_t") {
+    res[
+      c("alpha", "rho", "nu", "delta", "theta", "pi_F", "pi_t", "pi_G", "pi_C", "p"), 
+      value := c(htest$parameter, htest$p.value)
+      ]
+  }
+  
+  # set all other weights of non-mixture copulas to zero
+  res[grepl("pi+", variable) & is.na(value), value := 0]
+  
+  res <- dcast(res, pair + regime ~ variable)
+  return(res)
+}
+
+res_av <- rbindlist(lapply(gof_av_res, function(x) gof_res_row(x, NA_character_, NA_character_)))
+
+res_av[, pair := sapply(gof_av, function(x) x@pairname)]
+res_av[, regime := sapply(gof_av, function(x) x@regime)]
+
+res_av[, c1 := stringr::str_extract(pair, "^\\w{3}")]
+res_av[, c2 := stringr::str_extract(pair, "\\w{3}$")]
+
+res_av[, r1 := stringr::str_extract(regime, "high")]
+res_av[, r2 := stringr::str_extract(regime, "low")]
+
+done <- rep(NA_character_, length(unique(res_av$c1)))
+for(c in unique(res_av$c1)) {
+  res_av[!(c1 %in% done) & c2 == c, c("c1", "c2", "r1", "r2") := .(c2, c1, r2, r1)]
+  done[which(is.na(done))[1]] <- c
+}
+
+setorder(res_av, c1, c2, r1)
+
+res_av[, pair := paste(c1, c2, collapse = "-")]
+res_av[, k := ifelse(r1 == "high", 2, 3)]
+
+res_av[, c("c1", "c2", "r1", "r2", "regime") := NULL]
+setcolorder(res_av, c("pair", "k"))
--- scripts/08_tables_tex.R
+++ scripts/08_tables_tex.R
@@ -12,6 +12,7 @@ library(data.table)
 library(moments)
 library(countrycode)
 library(stringr)
+library(copula)
 
 
 # Load data ---------------------------------------------------------------
@@ -39,7 +40,6 @@ caption_at_bottom <- function(x) {
   cat(res, "\n")
   invisible(res)
 }
-
 
 
 # Descriptives -- Returns -------------------------------------------------
@@ -132,3 +132,166 @@ cvm_gpd_sg <- str_replace(
 cvm_gpd_sg <- str_replace(cvm_gpd_sg, "p-value", "$p$-value")
 cvm_gpd_sg <- str_remove(cvm_gpd_sg, "Country")
 cat(cvm_gpd_sg, sep = "\n")
+
+
+# GoF results -------------------------------------------------------------
+
+cop_res <- setClass(
+  "cop_res", slots = list(
+    pairname = "character", regime = "character", x = "matrix", u = "matrix", 
+    copula = "fitCopula"
+  )
+)
+
+load("./data/tmp/gof_res.RData")
+load("./data/tmp/gof_av_res.RData")
+
+gof_res_row <- function(htest, pair, regime) {
+  # function to generate row for LaTeX results table of GoF results
+  # Args:
+  #   htest: object of class htest containing the GoF results
+  #   pair: character, name of country pair
+  #   regime: character, volatility regime
+  # Returns:
+  #   data.table containing the results
+  
+  c <- stringr::str_extract(
+    htest$method, 
+    "t-copula|Gumbel|Frank|Clayton|Frnc, Gmbc, Clyc|Frnc, t-cp, Gmbc, Clyc"
+  )
+  if(!(c %in% c("Gumbel", "Frank", "Clayton"))) {
+    c <- switch(
+      c, 
+      "t-copula" = "t",
+      "Frnc, Gmbc, Clyc" = "m_nt",
+      "Frnc, t-cp, Gmbc, Clyc" = "m_t"
+    ) 
+  }
+  
+  res <- data.table(
+    pair = pair, regime = regime, pi_F = NA_real_, pi_G = NA_real_, 
+    pi_C = NA_real_, pi_t = NA_real_, alpha = NA_real_, delta = NA_real_, 
+    theta = NA_real_, rho = NA_real_, nu = NA_real_, p = NA_real_
+  )
+  
+  res <- melt(res, id.vars = c("pair", "regime"))
+  setkey(res, variable)
+  
+  if(c == "t") {
+    res[
+      c("pi_t", "rho", "nu", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Gumbel") {
+    res[
+      c("pi_G", "delta", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Clayton") {
+    res[
+      c("pi_C", "theta", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "Frank") {
+    res[
+      c("pi_G", "alpha", "p"), 
+      value := c(1, htest$parameter, htest$p.value)
+      ]
+  } else if(c == "m_nt") {
+    res[
+      c("alpha", "delta", "theta", "pi_F", "pi_G", "pi_C", "p"), 
+      value := c(htest$parameter, htest$p.value)
+      ]
+  } else if(c == "m_t") {
+    res[
+      c("alpha", "rho", "nu", "delta", "theta", "pi_F", "pi_t", "pi_G", "pi_C", "p"), 
+      value := c(htest$parameter, htest$p.value)
+      ]
+  }
+  
+  # set all other weights of non-mixture copulas to zero
+  res[grepl("pi+", variable) & is.na(value), value := 0]
+  
+  res <- dcast(res, pair + regime ~ variable)
+  return(res)
+}
+
+## asymmetric volatility
+gof_tab_av <- rbindlist(
+  lapply(gof_av_res, function(x) gof_res_row(x, NA_character_, NA_character_))
+  )
+
+gof_tab_h <- rbindlist(
+  lapply(gof_res$high, function(x) gof_res_row(x, NA_character_, NA_character_))
+  )
+
+gof_tab_h[, pair := sapply(gof$high, function(x) x@pairname)]
+gof_tab_h[, regime := "high-high"]
+
+gof_tab_av[, pair := sapply(gof_av, function(x) x@pairname)]
+gof_tab_av[, regime := sapply(gof_av, function(x) x@regime)]
+# helper variables for sorting
+gof_tab_av[, c1 := stringr::str_extract(pair, "^\\w{3}")]
+gof_tab_av[, c2 := stringr::str_extract(pair, "\\w{3}$")]
+gof_tab_av[, r1 := stringr::str_extract(regime, "high")]
+gof_tab_av[, r2 := stringr::str_extract(regime, "low")]
+
+# sort to have same pair, different regime in consecutive rows
+done <- rep(NA_character_, length(unique(gof_tab_av$c1)))
+for(c in unique(gof_tab_av$c1)) {
+  gof_tab_av[
+    !(c1 %in% done) & c2 == c, 
+    c("c1", "c2", "r1", "r2") := .(c2, c1, r2, r1)
+    ]
+  done[which(is.na(done))[1]] <- c
+}
+setorder(gof_tab_av, c1, c2, r1)
+
+gof_tab_av[, pair := paste0(c1, "-", c2)]
+gof_tab_av[, k := ifelse(r1 == "high", 2, 3)]
+gof_tab_av[, c("c1", "c2", "r1", "r2", "regime") := NULL]
+setcolorder(gof_tab_av, c("pair", "k"))
+
+gof_tab_h[, k := 4]
+gof_tab_h[, regime := NULL]
+
+gof_tab <- rbindlist(list(gof_tab_av, gof_tab_h), use.names = TRUE)
+setorder(gof_tab, pair, k)
+
+# stargazer
+gof_sg <- stargazer(
+  gof_tab, 
+  summary = FALSE,
+  label = "tab:gof_res_av",
+  rownames = FALSE,
+  digits = 2
+)
+
+gof_sg <- caption_at_bottom(gof_sg)
+
+gof_sg <- str_replace_all(gof_sg, "pi\\\\_(\\w{1})","$\\\\pi_\\1$")
+gof_sg <- str_replace_all(gof_sg, "\\\\_","_")
+gof_sg <- str_replace(gof_sg, "[&] k [&]","& $k$ &")
+gof_sg <- str_replace(gof_sg, "pair", "Pair")
+gof_sg <- str_replace(gof_sg, " p ", " $p$-value ")
+gof_sg <- str_replace(gof_sg, "\\\\centering", "\\\\centering \\\\footnotesize")
+
+for(s in c("alpha", "delta", "theta", "rho", "nu")) {
+  gof_sg <- str_replace(gof_sg, s, paste0("$\\\\", s, "$"))
+}
+
+gof_sg <- str_replace(
+  gof_sg, 
+  "caption[{][}]", 
+  paste("caption{Estimation results of best fitting copulas by market pair and", 
+        " volatility regime. The table gives the parameter estimates of the ", 
+        "single copulas (respective $\\\\pi$ equals unity), and for mixture ", 
+        "copulas. \\\\acs*{gof} test $p$-values are from 1000 bootstrap ", 
+        "replications of the $S_n^{(B)}$ test statistic of ", 
+        "\\\\citet{Genest:2009}.}", 
+        sep = "")
+)
+
+cat(gof_sg)
+
+
